c8695630de5724c50323f6f58e66a1f3de764408
Lanmi2304 <milanpavlovic413@gmail.com>
2024-10-29 16:16:51 +0100

    chore:  update next config

--- lib/config/app.config.ts
+++ lib/config/app.config.ts
@@ -1,64 +1,8 @@
-export const APP_NAME = 'senna sound';
-export const APP_DEFAULT_TITLE = 'Senna Sound';
-export const APP_TITLE_TEMPLATE = '%s - Senna Sound';
+export const APP_NAME = 'favonius ';
+export const APP_DEFAULT_TITLE = 'Favonius';
+export const APP_TITLE_TEMPLATE = '%s - Favonius';
 export const APP_DESCRIPTION =
   'Discover unmatched precision, transparency, and control in our high-end preamplifier—perfect for powering stereo systems or enhancing home audio setups.';
-export const APP_EMAIL = 'office@senna-sound.com';
-export const KEYWORDS = [
-  'Senna Sound',
-  'high-fidelity audio',
-  'audio amplifiers',
-  'line preamplifiers',
-  'power amplifiers',
-  'phono preamplifiers',
-  'premium audio',
-  'precision sound',
-  'audiophile equipment',
-  'high-end audio systems',
-  'Orca preamplifier',
-  'Onyx power amplifier',
-  'Phoenix phono stage',
-  'analog sound',
-  'MM and MC cartridges',
-  'low distortion',
-  'dynamic sound',
-  'noise reduction',
-  'clarity and transparency',
-  'premium materials',
-  'cutting-edge audio technology',
-  'immersive sound experience',
-  'amplifier design',
-  'amplifier specifications',
-  'RCA inputs',
-  'audiophile-grade performance',
-  'vinyl enthusiasts',
-];
-export const TAGS = [
-  'Senna Sound',
-  'high-fidelity audio',
-  'audio equipment',
-  'amplifiers',
-  'preamplifiers',
-  'power amplifiers',
-  'phono amplifiers',
-  'premium sound',
-  'audiophile',
-  'line preamplifier',
-  'power amplifier',
-  'phono stage',
-  'analog audio',
-  'vinyl',
-  'audio',
-  'MM cartridges',
-  'MC cartridges',
-  'distortion-free sound',
-  'dynamic audio',
-  'noise reduction',
-  'precision audio',
-  'home audio',
-  'high-end audio',
-  'audio technology',
-  'sound quality',
-  'luxury audio',
-  'audiophile-grade',
-];
+export const APP_EMAIL = 'favonius@example.com';
+export const KEYWORDS = [];
+export const TAGS = [];
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  experimental: {
+    typedRoutes: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
